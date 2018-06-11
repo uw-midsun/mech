843603e46c275ca99be10491febd6eddde58217d
devonkcc <devonkcc@gmail.com>
2018-06-10 22:57:58 -0700

    Updates to key vehicle classes
    impemented most of torque, and sunlight models

--- HighLevelCarModel/vehicle.py
+++ HighLevelCarModel/vehicle.py
@@ -21,6 +21,7 @@ __copyright__ = "Copyright 2018, Midnight Sun Solar Car Team"
 
 from datetime import datetime, timedelta
 import math
+from numpy import sign
 
 class VehicleConstants:
     """Class to store all unchanging vehicle parameters"""
@@ -187,21 +188,116 @@ class Motor:
     
     def __init__(self):
         self.torque = NaN          #current motor torque, Nm
-        self.power = NaN                 #current motor consumption, W
+        self.power = NaN           #current motor consumption, W
+        self.mu_s_tire = NaN
+        self.mu_k_tire = NaN
+        self.diam_wheel = NaN
+        self.stall_torque = NaN
+        self.no_load_speed = NaN
+        self.regen_gain = NaN
         
-    def update(self, commanded_throttle):
-        pass
+    def get_torque_from_throttle(commanded_throttle, curr_ang_velocity):
+        """Function to map throttle to motor torque. Assume linear model
+           untill torque speed curve can be input"""
+        torque_vs_speed_slope = - (self.stall_torque / self.no_load_speed)
+        speed_adjusted_max_torque = stall_torque + torque_vs_speed_slope * curr_velocity
+        return speed_adjusted_max_torque * commanded_throttle
+
+    def get_drive_efficiency(curr_ang_velocity):
+        """Function to get motor drive efficiency from torque and speed"""
+        return 0.8 #[IMPROVEMENT] Set as constant for now
+    
+    def get_regen_efficiency(curr_ang_velocity):
+        """Function to get motor regen braking efficiency from torque and speed"""
+        return 0.8 #[IMPROVEMENT] Set as constant for now
+        
+    def update(self, commanded_throttle, curr_velocity, single_wheel_normal_force_rear):
+        """Function to update motor toque"""
+        curr_ang_velocity = 2 * math.pi * curr_velocity / (math.pi * self.diam_wheel)
+        if commanded_throttle == 0: # Check for regen braking, use linear model 
+            self.torque = self.regen_gain * curr_ang_velocity
+            self.power = self.torque * curr_ang_velocity * get_regen_efficiency(curr_ang_velocity)
+        else: 
+            self.torque = get_torque_from_throttle(commanded_throttle, curr_ang_velocity)
+            self.power = self.torque * curr_ang_velocity * get_drive_efficiency(curr_ang_velocity)
+    
+        #[IMPROVEMENT] Could add logic to check for tire slipping here
     
 class CarDynamics:
     """Class for car dynamics block"""
     
+    TO_RADIANS = 0.0174533
+    A_GRAVITY = 9.81
+    
     def __init__(self):
         # lower indices are most recent
-        self.velocity = [0,0,0]              # velocity list, m/s
-        self.acceleration = [0,0,0]          # acceleration list, m/s^2
+        self.velocity = [0,0]              # velocity list, m/s
+        self.acceleration = [0,0]          # acceleration list, m/s^2
+        self.mass = NaN
+        self.c_viscous = NaN
+        self.c_coulombic = NaN
+        self.c_drag = NaN
+        self.wheel_rad = NaN
+        self.wheel_j = NaN
+        self.curr_road_gradient = NaN
+        self.curr_elevation = NaN
+        self.curr_motor_torque = NaN
+        self.curr_brake_torque = NaN
+        self.curr_air_temperature = NaN
         
-        def update(self, motor_torque, brake_torque):
+        def get_air_density(elevation, temperature):
             pass
+        
+        def acceleration_diff_equn(time, velocity): 
+            # Define list to store all acceleration compoents
+            accelerations = []
+            # Motor torque compoent
+            accelerations.append(self.curr_motor_torque /
+                                 (self.wheel_rad  * self.mass * self.wheel_j))
+            # Coulombic friction compoent
+            accelerations.append(-self.c_coulombic * numpy.sign(velocity) /
+                                 (self.wheel_rad  * self.mass * self.wheel_j))
+            # Viscous damping component
+            accelerations.append(-self.c_viscous * velocity * numpy.sign(velocity) / 
+                                 (self.mass * self.wheel_j))
+            # Mechanical brakes compoenent
+            accelerations.append(-self.curr_brake_torque /
+                                 (self.wheel_rad**2  * self.mass * self.wheel_j))
+            # Aerodynamic drag compoenent
+            accelerations.append(self.get_air_density(self.curr_elevation) *
+                                 self.c_drag * self.frontal_area * velocity**2 /
+                                 (self.mass * self.wheel_j))
+            # Gravity compoenent
+            accelerations.append(self.A_GRAVITY *
+                                 math.sin(self.curr_road_gradiant*self.TO_RADIANS) /
+                                 self.wheel_j)
+            # Return superposition of all acceleration components
+            return sum(accelerations)
+        
+        def rungeK_kutta_4(function, t, delta_t, y): 
+            k1 = delta_t * function(t, y);
+            k2 = delta_t * function(t+delta_t/2, y+k1/2);
+            k3 = delta_t * function(t+delta_t/2, y+k2/2);
+            k4 = delta_t * function(t+delta_t, y+k3);
+            y_next = y + (k1 + 2*k2 + 2*k3 + k4)/6;
+            return y_next 
+        
+        def update(self, motor_torque, brake_torque, time, road_gradient):
+            slef.road_gradient = road_gradient
+            self.curr_motor_torque = motor_torque
+            self.curr_brake_torque = brake_torque
+            # Remove previous, previous values
+            self.accceleration.pop(0)
+            self.velocity.pop(0)
+            # Calcualte current acceleration using 4th order runge kutta
+            self.acceleration.append( self.rungeK_kutta_4(
+                                            self.acceleration_diff_equn,
+                                            time,
+                                            self.timestep,
+                                            self.velocity[1]) )
+            # Integerate using trapazoidal rule to find next velocity
+            self.velocity.append( self.velocity[0] + ( self.acceleration[0] +
+                                                       self.acceleration[1] * self.timestep / 2 ))
     
 class Array:
     """Class for array block"""
@@ -215,10 +311,51 @@ class Array:
 class Sunlight:
     """Class for sunlight block"""
     
+    TO_DEGREES = 57.2957549575 
+    
     def __init__(self):
         self.irradiance = NaN            #curent irradiance on car, W/m^2
-        
-    def update(self, position, elevation, cloudienss, date_time):
+    
+    def get_declination(date_time):
+        # Implement's Spencer's equation for declination (1971)
+        n = date_time.timetuple().tm_day # find n'th day of the year
+        B = (n-1)*(360 /365)
+        declination = (180/math.pi)*(0.006918 - 0.399912*math.cos(B)
+                      + 0.070257*math.sin(B) - 0.006758*math.cos(2*B)
+                      + 0.000907*math.sin(2*B) - 0.002697*cos(3*B)
+                      + 0.00148*sin(3*B))
+        return declination
+    
+    def get_surface_azimuth(heading):
+        return heading - 180
+    
+    def get_hour_angle(date_time):
+        # The hour angle is 15 degrees per hour before or after noon,
+        # morning negative, afternoon positive
+        return 15 * (date_time.hour - 12)
+    
+    def get_zinuth(date_time, position, heading):
+        # Uses formula from Solar Engineerin of Thermal Processes (2013) Duffie et al
+        lat = position.latitude
+        dclitn = get_declination(date_time)
+        hr_ang = get_hour_angle(date_time)
+        return math.acos( math.cos(lat) * math.cos(dclitn) * math.cos(hr_ang)
+                         + math.sin(lat) * math.sin(dclitn) )
+    
+    def get_angle_of_incidence(date_time, position, heading, gradient):
+        # Uses formula from Solar Engineerin of Thermal Processes (2013) Duffie et al
+        lat = position.latitude
+        dclitn = get_declination(date_time)
+        slope = math.atan(gradient) * TO_DEGREES
+        hr_ang = get_hour_angle(date_time)
+        azimuth = get_surface_azimuth(heading)
+        ang_inc = math.acos ( math.sin(dclitn) * math.sin(lat) * math.cos(slope)
+                        - math.sin(dclitn) * math.cos(lat) * math.sin(slope) * math.cos(azimuth)
+                        + cos(dclitn) * cos(lat) * cos(slope) * cos(hr_ang)
+                        + cos(dclitn) * sin(lat) * sin(slope) * cos(azimuth) * cos(hr_ang)
+                        + cos(dclitn) * sin(slope) * sin(azimuth) * sin(hr_ang) )
+          
+    def update(self, position, elevation, gradient, heading, cloudienss, date_time):
         pass
     
 class HighVoltageLosses:
@@ -345,8 +482,8 @@ class Vehicle:
 
     def update_vehicle_state(self):
         """Function to..."""
-        self.sunlight.update(self.position, self.elevation,
-                             self.cloudiness, self.date_time)
+        self.sunlight.update(self.position, self.elevation, self.gradient,
+                             self.heading, self.cloudiness, self.date_time)
         self.front_array.update(self.sunlight.irradiance)
         self.rear_array.update(self.sunlight.irradiance)
         self.driver.update(self.car_dynamics.velocity,
